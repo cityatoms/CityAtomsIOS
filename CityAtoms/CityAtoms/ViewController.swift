48b972f93321c0a3f4e8e99a37504a6da0aa1cba
Feras Alnatsheh <hi@fer.as>
2020-04-01 20:33:30 -0400

    Add new project

---
+++ CityAtoms/CityAtoms/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CityAtoms
+//
+//  Created by Feras Alnatsheh on 4/1/20.
+//  Copyright © 2020 City Atoms. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
