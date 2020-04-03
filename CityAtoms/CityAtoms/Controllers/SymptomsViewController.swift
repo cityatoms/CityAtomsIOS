5a6292e1d6b24dd603ef8f40c0b984bd8afe09e2
Mouzzam Tahir <moazzamtahir81@gmail.com>
2020-04-03 16:30:46 +0500

    added connections and tap gesture for the cells

--- CityAtoms/CityAtoms/Controllers/SymptomsViewController.swift
+++ CityAtoms/CityAtoms/Controllers/SymptomsViewController.swift
@@ -17,18 +17,65 @@ class SymptomsViewController: UIViewController {
     @IBOutlet var cell5: UIView!
     @IBOutlet var cell6: UIView!
     
+    @IBOutlet var saveSymptomsButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        if let image = cell1.viewWithTag(1) {
-            let img = image as! UIImageView
-            print(img.image?.size)
-        }
-        
-        if let image = cell2.viewWithTag(2) {
-            print("Found \(image as! UILabel)")
-        }
     }
     
+    func initializeUI() {
+        let tapGesture1 = UITapGestureRecognizer.init(target: self, action: #selector(selectCell1))
+        cell1.isUserInteractionEnabled = true
+        cell1.addGestureRecognizer(tapGesture1)
+        
+        let tapGesture2 = UITapGestureRecognizer.init(target: self, action: #selector(selectCell2))
+        cell2.isUserInteractionEnabled = true
+        cell2.addGestureRecognizer(tapGesture2)
+        
+        let tapGesture3 = UITapGestureRecognizer.init(target: self, action: #selector(selectCell3))
+        cell3.isUserInteractionEnabled = true
+        cell3.addGestureRecognizer(tapGesture3)
+        
+        let tapGesture4 = UITapGestureRecognizer.init(target: self, action: #selector(selectCell4))
+        cell4.isUserInteractionEnabled = true
+        cell4.addGestureRecognizer(tapGesture4)
+    
+        let tapGesture5 = UITapGestureRecognizer.init(target: self, action: #selector(selectCell5))
+        cell5.isUserInteractionEnabled = true
+        cell5.addGestureRecognizer(tapGesture5)
+        
+        let tapGesture6 = UITapGestureRecognizer.init(target: self, action: #selector(selectCell6))
+        cell6.isUserInteractionEnabled = true
+        cell6.addGestureRecognizer(tapGesture6)
+    }
+   
+    @objc
+    func selectCell1() {
+    }
+    
+    @objc
+    func selectCell2() {
+    }
+    
+    @objc
+    func selectCell3() {
+    }
+    
+    @objc
+    func selectCell4() {
+    }
+    
+    @objc
+    func selectCell5() {
+    }
+    
+    @objc
+    func selectCell6() {
+    }
+    
+    @IBAction func saveSymptoms(_ sender: Any) {
+        
+    }
     
 }
